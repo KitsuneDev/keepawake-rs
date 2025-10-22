58902c8d1af190b49724a0db367b5b68344b385c
Kitsune <kitsune@akitsune.dev>
2025-10-22 00:25:48 -0400

    Update shadow-rs

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    shadow_rs::new()?;
+    shadow_rs::ShadowBuilder::builder().build().unwrap();
 
     if cfg!(target_os = "windows") {
         winres::WindowsResource::new()
